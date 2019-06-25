a51376817de32e432fc47f6311600e3b7784e9b9
dieterjava <md_hermann@yahoo.de>
2019-06-25 18:40:35 +0200

    7 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    console.log('environment production = ' + environment.production);
+    console.log('animal  = ' + environment.animal);
+  }
 
   ngOnInit() {
   }
