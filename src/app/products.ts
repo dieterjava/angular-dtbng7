05ee11ace9091311cb6d97a5c7b511cd2db3d438
dieterjava <md_hermann@yahoo.de>
2020-02-19 11:39:36 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,12 +2,12 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with the best screens'
+    description: 'A large phone with the best screens, we think'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with  the best cameras'
+    description: 'A great phone with  the best cameras, we think'
   },
   {
     name: 'Phone Standard',
